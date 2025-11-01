efb63255f2ed710b22713877a2d29b73be3127ad
Jasonchen52011 <chenhaoxiang520119@gmail.com>
2025-11-01 09:49:07 +0800

    fix: remove Html tags from global not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,13 +3,9 @@
  */
 export default function GlobalNotFound() {
   return (
-    <html lang="en">
-      <body>
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
-          <h1>404 - Page Not Found</h1>
-          <p>The page you are looking for does not exist.</p>
-        </div>
-      </body>
-    </html>
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
   );
 }
